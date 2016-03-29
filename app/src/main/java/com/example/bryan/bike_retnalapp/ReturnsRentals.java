ddcd574c1728eb813d4d2567b3d093759b43e1e2
Bryaners <lackard@gmail.com>
2016-03-29 18:19:13 +0100

    Returns Page page for main campus working user enters return code to lodge bike back

--- app/src/main/java/com/example/bryan/bike_retnalapp/ReturnsRentals.java
+++ app/src/main/java/com/example/bryan/bike_retnalapp/ReturnsRentals.java
@@ -1,13 +1,25 @@
 package com.example.bryan.bike_retnalapp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 public class ReturnsRentals extends AppCompatActivity {
+
+    protected Button mContinue;
+    protected Button mBack;
+    protected TextView mReturnNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,19 +28,117 @@ public class ReturnsRentals extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                      //  .setAction("Action", null).show();
+
+                Intent takeUserToRentBike = new Intent(ReturnsRentals.this, HomePageActivity.class);
+                startActivity(takeUserToRentBike);
+            }
+        });
+
+//////////////////////////////////////////////////////////////////////
+
+        //real stuff here
+        mContinue =(Button)findViewById(R.id.continueWithReturn);
+        mBack =(Button)findViewById(R.id.backToMainCampus);
+        mReturnNum = (EditText) findViewById(R.id.ReturnNumBox);
+
+
+        mBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+               // Toast.makeText(ReturnsRentals.this, "BACK BTN", Toast.LENGTH_LONG).show();
+                Intent takeUserToRentBike = new Intent(ReturnsRentals.this, MainCampusActivity.class);
+                startActivity(takeUserToRentBike);
             }
         });
 
 
+        mContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                String returnNumber;
+                 returnNumber = mReturnNum.getText().toString().trim();
 
+                //Toast.makeText(ReturnsRentals.this, "Continuye click", Toast.LENGTH_LONG).show();
 
+                if (returnNumber.isEmpty()) {
+
+                    AlertDialog.Builder buidler = new AlertDialog.Builder(ReturnsRentals.this);
+                    buidler.setMessage("No Return code was entered Sorry!");
+                    buidler.setTitle("Oops!");
+                    buidler.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int which) {
+                            //close the dialog when button pressed
+                            dialogInterface.dismiss();
+                        }
+                    });
+                    AlertDialog dialog = buidler.create();
+                    dialog.show();
+
+                }else if(returnNumber.equals("1234") ){  //== "1234" wouldnt work? unsure why
+
+                    ParseUser currentUser = ParseUser.getCurrentUser();
+                    String currentUserUsername = currentUser.getUsername();
+
+                    ParseObject returningBikeMain = new ParseObject("Transaction");
+                    returningBikeMain.put("NameOfBike", "Bike");
+                    returningBikeMain.put("Campus", "Main Campus");
+                    returningBikeMain.put("user", currentUserUsername);
+                    returningBikeMain.saveInBackground();
+
+                    Intent takeUserToRentBike = new Intent(ReturnsRentals.this, MainCampusActivity.class);
+                    startActivity(takeUserToRentBike);
+
+            }
+                else if(returnNumber.equals("4444")){
+
+                    ParseUser currentUser = ParseUser.getCurrentUser();
+                    String currentUserUsername = currentUser.getUsername();
+
+                    ParseObject returningBikeMain = new ParseObject("Transaction");
+                    returningBikeMain.put("NameOfBike", "Bike");
+                    returningBikeMain.put("Campus", "Main Campus");
+                    returningBikeMain.put("user", currentUserUsername);
+                    returningBikeMain.saveInBackground();
+
+                    Intent takeUserToRentBike = new Intent(ReturnsRentals.this, MainCampusActivity.class);
+                    startActivity(takeUserToRentBike);
+
+                }
+                else{
+                    AlertDialog.Builder buidler = new AlertDialog.Builder(ReturnsRentals.this);
+                    buidler.setMessage("An incorrect return code has been entered Sorry!");
+                    buidler.setTitle("Please try again!");
+                    buidler.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int which) {
+                            //close the dialog when button pressed
+                            dialogInterface.dismiss();
+                        }
+                    });
+                    AlertDialog dialog = buidler.create();
+                    dialog.show();
+                }
+
+        }
     }
+
+
+    );
+
+
+
+
+
+    }//end of on create
 
 }
